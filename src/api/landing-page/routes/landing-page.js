2deb7b1b7400c824136c701b4fde0702586cee54
Anish1411 <kirodiwalanish11@gmail.com>
2024-06-06 12:27:27 +0530

    E-commerce-project

---
+++ src/api/landing-page/routes/landing-page.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const middlewares = require("../../../../config/middlewares");
+
+/**
+ * landing-page router
+ */
+
+const { createCoreRouter } = require("@strapi/strapi").factories;
+
+module.exports = createCoreRouter("api::landing-page.landing-page", {
+  config: {
+    find: {
+      middlewares: ["api::landing-page.landing-page-populate"],
+    },
+    findOne: {
+      middlewares: ["api::landing-page.landing-page-populate"],
+    },
+  },
+});
